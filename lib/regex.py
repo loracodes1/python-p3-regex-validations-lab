da15fabf0703095afff1c25f02565e672ab87a1d
florence <loracodes3@gmail.com>
2024-12-08 22:25:36 +0300

    commit

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,14 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Regular expression for validating names
+name = r"^[A-Z][a-z]*(?:'[A-Za-z]+)?(?: [A-Z][a-z]*(?:-[A-Z][a-z]*)?)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# Regular expression for validating phone numbers
+phone_number = r"^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# Regular expression for validating email addresses
+# Ensures the email starts with a letter
+email_address = r"^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 email_regex = re.compile(email_address)
